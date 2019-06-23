79b95b1666783bdac3bad762338be9c51be1dce3
久远 <48052435+ykomashiro@users.noreply.github.com>
2019-06-23 10:09:59 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -5,7 +5,7 @@ from keras_alfnet.model.model_2step import Model_2step
 
 # pass the settings in the config object
 C = config.Config()
-os.environ["CUDA_VISIBLE_DEVICES"] = '2'
+os.environ["CUDA_VISIBLE_DEVICES"] = ''
 
 # define paths for weight files and detection results
 w_path = 'data/models/city_res50_2step.hdf5'
